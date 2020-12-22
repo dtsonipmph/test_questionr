0b84305f8310954143b73069f0832954076aac20
dtsonipmph <69644762+dtsonipmph@users.noreply.github.com>
2020-12-22 17:05:36 +0700

    Add files via upload

--- R/icut.R
+++ R/icut.R
@@ -343,7 +343,7 @@ icut <- function(obj = NULL, var_name = NULL) {
         observeEvent(input$done, {
           ## Generate code
           out <- generate_code()
-          out
+          cat(out)
           }
           stopApp()
         })
